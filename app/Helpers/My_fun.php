1e6fa5940e5774f4a4e8ac3e06e7e6e76c270da9
Alan Lengkoan <alanlengkoan15@gmail.com>
2023-12-23 00:01:46 +0800

    update again

--- app/Helpers/My_fun.php
+++ app/Helpers/My_fun.php
@@ -544,11 +544,10 @@ if (!function_exists('count_progress')) {
             }
 
             $total_volume = round($volume, 2);
-            // $count_volume = (@(($total_volume * $harga_satuan) / $total_kontrak) * 100);
-            $count_volume = 0;
+            $count_volume = @((($total_volume * $harga_satuan) / $total_kontrak) * 100);
 
             if ($total_volume > 0) {
-                $result += $count_volume;
+                $result += is_nan($count_volume) ? 0 : $count_volume;
             }
         }
         
