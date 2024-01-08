1d576dfba2811c44fbdf6bb3fb93de8601af9a0e
Alan Lengkoan <alanlengkoan15@gmail.com>
2024-01-08 14:21:44 +0800

    laporan kontrak

--- app/Exports/KontrakExport.php
+++ app/Exports/KontrakExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Kontrak;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class KontrakExport implements FromView
+class KontrakExport implements FromView, ShouldAutoSize
 {
     public function view(): View
     {
--- resources/views/admin/kontrak/excel.blade.php
+++ resources/views/admin/kontrak/excel.blade.php
@@ -1,39 +1,77 @@
 <table>
     <thead>
         <tr>
-            <th rowspan="2">NAMA PEKERJAAN</th>
-            <th rowspan="2">KODE RUP</th>
-            <th rowspan="2">NILAI PAGU (Rp.)</th>
-            <th rowspan="2">NAMA PENYEDIA</th>
-            <th rowspan="2">NAMA DIREKTUR</th>
-            <th rowspan="2">ALAMAT PERUSAHAAN</th>
-            <th colspan="2">BA. MC NOL</th>
-            <th colspan="2">BA. NEGO</th>
-            <th colspan="2">SPPBJ</th>
-            <th colspan="2">BA. PERSIAPAN PENANDATANGANAN</th>
-            <th colspan="2">SURAT PESANAN</th>
-            <th colspan="3">SURAT PERJANJIAN / SPK</th>
+            <th align="center" valign="middle" rowspan="2"><b>NAMA PEKERJAAN</b></th>
+            <th align="center" valign="middle" rowspan="2"><b>KODE RUP</b></th>
+            <th align="center" valign="middle" rowspan="2"><b>NILAI PAGU (Rp.)</b></th>
+            <th align="center" valign="middle" rowspan="2"><b>NAMA PENYEDIA</b></th>
+            <th align="center" valign="middle" rowspan="2"><b>NAMA DIREKTUR</b></th>
+            <th align="center" valign="middle" rowspan="2"><b>ALAMAT PERUSAHAAN</b></th>
+            <th align="center" colspan="2"><b>BA. MC NOL</b></th>
+            <th align="center" colspan="2"><b>BA. NEGO</b></th>
+            <th align="center" colspan="2"><b>SPPBJ</b></th>
+            <th align="center" colspan="2"><b>BA. PERSIAPAN PENANDATANGANAN</b></th>
+            <th align="center" colspan="2"><b>SURAT PESANAN</b></th>
+            <th align="center" colspan="3"><b>SURAT PERJANJIAN / SPK</b></th>
+            <th align="center" colspan="2"><b>SPMK</b></th>
+            <th align="center" colspan="3"><b>JANGKA WAKTU PELAKSANAAN</b></th>
+            <th align="center" colspan="2"><b>BA. PENYERAHAN LOKASI PEKERJAAN</b></th>
+            <th align="center" valign="middle" rowspan="2"><b>PPTK</b></th>
+            <th align="center" valign="middle" rowspan="2"><b>PEMBUAT KONTRAK</b></th>
         </tr>
         <tr>
-            <th>NOMOR</th>
-            <th>TANGGAL</th>
-            <th>NOMOR</th>
-            <th>TANGGAL</th>
-            <th>NOMOR</th>
-            <th>TANGGAL</th>
-            <th>NOMOR</th>
-            <th>TANGGAL</th>
-            <th>NOMOR</th>
-            <th>TANGGAL</th>
-            <th>NOMOR</th>
-            <th>TANGGAL</th>
-            <th>NILAI KONTRAK</th>
+            <th align="center"><b>NOMOR</b></th>
+            <th align="center"><b>TANGGAL</b></th>
+            <th align="center"><b>NOMOR</b></th>
+            <th align="center"><b>TANGGAL</b></th>
+            <th align="center"><b>NOMOR</b></th>
+            <th align="center"><b>TANGGAL</b></th>
+            <th align="center"><b>NOMOR</b></th>
+            <th align="center"><b>TANGGAL</b></th>
+            <th align="center"><b>NOMOR</b></th>
+            <th align="center"><b>TANGGAL</b></th>
+            <th align="center"><b>NOMOR</b></th>
+            <th align="center"><b>TANGGAL</b></th>
+            <th align="center"><b>NILAI KONTRAK (Rp.)</b></th>
+            <th align="center"><b>NOMOR</b></th>
+            <th align="center"><b>TANGGAL</b></th>
+            <th align="center"><b>JANGKA WAKTU</b></th>
+            <th align="center"><b>MULAI</b></th>
+            <th align="center"><b>SELESAI</b></th>
+            <th align="center"><b>NOMOR</b></th>
+            <th align="center"><b>TANGGAL</b></th>
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td>{{ $kontrak->toPaket->nama }}</td>
+            <td>{{ $kontrak->toPaket->toKegiatan->nama }} {{ $kontrak->toPaket->nama }}</td>
             <td>{{ $kontrak->kd_rekening }}</td>
+            <td>{{ rupiah($kontrak->nil_pagu) }}</td>
+            <td>{{ $kontrak->toPenyedia->nama }}</td>
+            <td>{{ $kontrak->pj_penyedia }}</td>
+            <td>{{ $kontrak->toPenyedia->alamat }}</td>
+            <td>{{ $kontrak->no_ba_mc0 }}</td>
+            <td>{{ tgl_indo($kontrak->tgl_ba_mc0) }}</td>
+            <td>{{ $kontrak->no_ba_kntb }}</td>
+            <td>{{ tgl_indo($kontrak->tgl_ba_kntb) }}</td>
+            <td>{{ $kontrak->no_sppbj }}</td>
+            <td>{{ tgl_indo($kontrak->tgl_sppbj) }}</td>
+            <td>{{ $kontrak->no_ba_rp2k }}</td>
+            <td>{{ tgl_indo($kontrak->tgl_ba_rp2k) }}</td>
+            <td>{{ $kontrak->no_sp }}</td>
+            <td>{{ tgl_indo($kontrak->tgl_sp) }}</td>
+            <td>{{ $kontrak->no_kontrak }}</td>
+            <td>{{ tgl_indo($kontrak->tgl_kontrak) }}</td>
+            <td>{{ rupiah($kontrak->nil_kontrak) }}</td>
+            <td>{{ $kontrak->no_spmk }}</td>
+            <td>{{ tgl_indo($kontrak->tgl_spmk) }}</td>
+            <td>{{ count_day_excluding_weekends_holiday( $kontrak->tgl_kontrak_akhir, $kontrak->tgl_kontrak_mulai) . ' HK'; }}</td>
+            <td>{{ tgl_indo($kontrak->tgl_kontrak_mulai) }}</td>
+            <td>{{ tgl_indo($kontrak->tgl_kontrak_akhir) }}</td>
+            <td>{{ $kontrak->no_ba_plp }}</td>
+            <td>{{ tgl_indo($kontrak->tgl_ba_plp) }}</td>
+            <td>{{ $kontrak->toPaket->toKegiatan->toPptk->toUser->nama }}</td>
+            <td>{{ $kontrak->pembuat_kontrak }}</td>
         </tr>
     </tbody>
 </table>
